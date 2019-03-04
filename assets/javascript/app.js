e1e41d115f84c7bfa40c1544061c5a43e47f5175
whatevermane <zmatt33@gmail.com>
2019-03-04 16:08:51 -0600

    Added basic HTML and arrays of rappers

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  var topics = [
+    "run dmc",
+    "notorious big",
+    "tupac",
+    "nwa",
+    "mc hammer",
+    "eazy e",
+    "wu-tang clan",
+    "sir mixalot",
+    "master p",
+    "beastie boys"
+  ];
+
+  var listOfOthers = [
+    "dmx",
+    "three 6 mafia",
+    "nas",
+    "dr.dre",
+    "salt n pepa",
+    "jay z",
+    "warren g",
+    "lil wayne",
+    "mob deep",
+    "krs one",
+    "de la soul"
+  ];
+});
