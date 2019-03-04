c56f55ce1f5c3f09a452ae606d9cba8f7b24f4e5
whatevermane <zmatt33@gmail.com>
2019-03-04 16:23:46 -0600

    Added function to render buttons-not sure if working. Will test shortly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,27 @@
 $(document).ready(function() {
+    renderButtons();
+});
+
   var topics = [
     "run dmc",
     "notorious big",
-    "tupac",
+    "2pac",
     "nwa",
     "mc hammer",
     "eazy e",
     "wu-tang clan",
     "sir mixalot",
     "master p",
-    "beastie boys"
+    "beastie boys",
+    "outkast",
   ];
+  var lowerCaseList = [];
 
   var listOfOthers = [
     "dmx",
     "three 6 mafia",
     "nas",
-    "dr.dre",
+    "dr. dre",
     "salt n pepa",
     "jay z",
     "warren g",
@@ -24,5 +29,29 @@ $(document).ready(function() {
     "mob deep",
     "krs one",
     "de la soul"
+    "lil jon",
+    "kanye west",
+    "snoop dogg"
+    "ice cube",
+    "ugk",
+    "kool keith",
+    ""
   ];
-});
+  for (var i = 0; i < listOfOthers.length; i++) {
+    lowerCaseList.push(listOfOthers[i].toLowerCase());
+  }
+
+function renderButtons() {
+$("#rapper-button").empty();
+
+for (var i=0; i < topics.length; i++) {
+ var rapper = topics[i];
+ var button = $("<button class = 'rapper m-1 p- btn btn-warning'/>")
+ .attr("data-name", rapper)
+      .text(rapper);
+      $("#rapper-button").append(button);
+  }
+}
+
+$(document).on("click", ".animal", getApi);
+
